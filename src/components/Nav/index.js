77d98ea17509d1b3b7c54438365c5f8e1ebb65de
bluesatyr <shawnevans.music@gmail.com>
2021-04-24 15:58:36 -0400

    create tests for ContactForm

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -11,7 +11,7 @@ function Nav(props) {
   } = props;
 
   useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
+    document.title = capitalizeFirstLetter({currentCategory.name});
   }, [currentCategory]);
 
   const handleClick = (item) => {
